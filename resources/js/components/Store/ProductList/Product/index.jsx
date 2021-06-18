0109cebb3654bfcfd815c531a7e2c8417f5e1731
Alan Sanchez <realalansan@gmail.com>
2021-06-18 11:46:47 -0700

    Fix flexbox

--- resources/js/components/Store/ProductList/Product/index.jsx
+++ resources/js/components/Store/ProductList/Product/index.jsx
@@ -4,8 +4,10 @@ const index = ({ product }) => {
     return (
         <a className="product" href="/product">
             <img className="image" src={product.image} alt={product.name} />
-            <p className="name">{product.name}</p>
-            <p className="price">${product.price}</p>
+            <div>
+                <p className="name">{product.name}</p>
+                <p className="price">${product.price}</p>
+            </div>
         </a>
     );
 };
