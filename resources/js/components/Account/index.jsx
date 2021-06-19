0fd0ab7ecef780653cce799ccffec5569c99a065
Alan Sanchez <realalansan@gmail.com>
2021-06-19 08:05:28 -0700

    Add account component

--- resources/js/components/Account/index.jsx
+++ resources/js/components/Account/index.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React from "react";
 
 const index = () => {
     return (
-        <div>
-            
+        <div className="account">
+            <h1 className="heading">Account</h1>
+            <h3 className="productsHeading">Your Products</h3>
+            <div className="account-info">
+                <h4 className="name subheading">Name</h4>
+                <h6 className="name">Alan</h6>
+                <h4 className="email subheading">Email</h4>
+                <h6 className="email">someone@gmail.com</h6>
+                <button className="sm primary">Log Out</button>
+            </div>
+            <div className="products">
+                <div className="product">
+                    <h3>T-Shirt</h3>
+                </div>
+            </div>
         </div>
-    )
-}
+    );
+};
 
-export default index
+export default index;
