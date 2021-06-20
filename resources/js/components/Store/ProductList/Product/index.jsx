f45a42545b2be3d4c425c3fc86821e99d31175ce
Alan Sanchez <realalansan@gmail.com>
2021-06-20 11:40:20 -0700

    Fix image styling

--- resources/js/components/Store/ProductList/Product/index.jsx
+++ resources/js/components/Store/ProductList/Product/index.jsx
@@ -3,7 +3,9 @@ import React from "react";
 const index = ({ product }) => {
     return (
         <a className="product" href="/product">
-            <img className="image" src={product.image} alt={product.name} />
+            <div className="image">
+                <img className="image" src={product.image} alt={product.name} />
+            </div>
             <div>
                 <p className="name">{product.name}</p>
                 <p className="price">${product.price}</p>
