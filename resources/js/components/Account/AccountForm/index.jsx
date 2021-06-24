81d73d8186f684d6dee1c671c6113dbff14ff0a4
Alan Sanchez <realalansan@gmail.com>
2021-06-24 15:38:20 -0700

    Fix form layout

--- resources/js/components/Account/AccountForm/index.jsx
+++ resources/js/components/Account/AccountForm/index.jsx
@@ -10,70 +10,96 @@ const index = () => {
     const [register, setRegister] = useState(true);
 
     return (
-        <div className="account formContainer">
-            <h2>Register</h2>
-            <form action="" className="accountForm">
-                <label className="required" htmlFor="name">
-                    Name
-                </label>
-                <br />
-                <input
-                    name="name"
-                    id="name"
-                    type="text"
-                    value={form.name}
-                    onChange={(e) => setForm({ ...form, name: e.target.value })}
-                />
-                <br />
-                <label className="required" htmlFor="email">
-                    Email
-                </label>
-                <br />
-                <input
-                    name="email"
-                    id="email"
-                    type="text"
-                    value={form.email}
-                    onChange={(e) =>
-                        setForm({ ...form, email: e.target.value })
-                    }
-                />
-                <label className="required" htmlFor="password">
-                    Password
-                </label>
-                <br />
-                <input
-                    name="password"
-                    id="password"
-                    type="text"
-                    value={form.password}
-                    onChange={(e) =>
-                        setForm({ ...form, password: e.target.value })
-                    }
-                />
-                <label className="required" htmlFor="passwordConfirmation">
-                    Confirm Password
-                </label>
-                <br />
-                <input
-                    name="passwordConfirmation"
-                    id="passwordConfirmation"
-                    type="text"
-                    value={form.passwordConfirmation}
-                    onChange={(e) =>
-                        setForm({
-                            ...form,
-                            passwordConfirmation: e.target.value,
-                        })
-                    }
-                />
-                <button className="primary" type="submit">
-                    Register
-                </button>
-                <button className="sm center link text">
-                    Already registered? Log in
-                </button>
-            </form>
+        <div className="formContainer">
+            <div className="accountForm">
+                <h2>{register ? "Register" : "Log In"}</h2>
+                <form action="POST" className="form">
+                    {register ? (
+                        <>
+                            <label className="required" htmlFor="name">
+                                Name
+                            </label>
+                            <br />
+                            <input
+                                name="name"
+                                id="name"
+                                type="text"
+                                value={form.name}
+                                onChange={(e) =>
+                                    setForm({ ...form, name: e.target.value })
+                                }
+                            />
+                            <br />
+                        </>
+                    ) : (
+                        ""
+                    )}
+                    <label className="required" htmlFor="email">
+                        Email
+                    </label>
+                    <br />
+                    <input
+                        name="email"
+                        id="email"
+                        type="text"
+                        value={form.email}
+                        onChange={(e) =>
+                            setForm({ ...form, email: e.target.value })
+                        }
+                    />
+                    <label className="required" htmlFor="password">
+                        Password
+                    </label>
+                    <br />
+                    <input
+                        name="password"
+                        id="password"
+                        type="text"
+                        value={form.password}
+                        onChange={(e) =>
+                            setForm({ ...form, password: e.target.value })
+                        }
+                    />
+                    {register ? (
+                        <>
+                            <label
+                                className="required"
+                                htmlFor="passwordConfirmation"
+                            >
+                                Confirm Password
+                            </label>
+                            <br />
+                            <input
+                                name="passwordConfirmation"
+                                id="passwordConfirmation"
+                                type="text"
+                                value={form.passwordConfirmation}
+                                onChange={(e) =>
+                                    setForm({
+                                        ...form,
+                                        passwordConfirmation: e.target.value,
+                                    })
+                                }
+                            />
+                        </>
+                    ) : (
+                        ""
+                    )}
+
+                    <button className="primary" type="submit">
+                        Register
+                    </button>
+                    <button
+                        className="sm center link text"
+                        type="button"
+                        onClick={() => setRegister((prev) => !prev)}
+                    >
+                        {register
+                            ? "Already registered? Log in here"
+                            : "Don't have an account? Register here"}
+                    </button>
+                </form>
+            </div>
         </div>
     );
 };
