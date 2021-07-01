8abf76f75e0e9cf5c19275071e3c331e97ab65cb
Alan Sanchez <realalansan@gmail.com>
2021-07-01 05:49:40 -0700

    Add product store method

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -5,6 +5,13 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Product;
 use Intervention\Image\Facades\Image;
+use PhpParser\Node\Stmt\TryCatch;
+
+use JWTAuth;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use Tymon\JWTAuth\Exceptions\TokenExpiredException;
+use Tymon\JWTAuth\Exceptions\TokenInvalidException;
+use Tymon\JWTAuth\Facades\JWTAuth as FacadesJWTAuth;
 
 class ProductsController extends Controller
 {
@@ -19,21 +26,39 @@ class ProductsController extends Controller
     {
         $data = request()->validate([
             'name' => 'required',
-            'image' => ['required'],
+            'image' => 'required',
             'price' => 'required|numeric',
             'category' => 'required',
             'discount' => 'numeric',
         ]);
 
-        $imagePath = request('image')->store('uploads', 'public');
+        // $imagePath = request('image')->store('uploads', 'public');
+        $imagePath = "123";
 
-        $image = Image::make(public_path("storage/{$imagePath}"))->fit(1200, 1200);
-        $image->save();
+        // $image = Image::make(public_path("storage/{$imagePath}"))->fit(1200, 1200);
+        // $image->save();
 
-        
+        $user = JWTAuth::user();
+
+        $product = $user->products()->create([
+            'name' => $data['name'],
+            'image' => $imagePath,
+            'price' => $data['price'],
+            'category' => $data['category'],
+            'discount' => $data['discount'],
+        ]);
+
+        return response()->json(compact('product'));
     }
 
     public function create()
     {
+    }
+
+    public function productsByUser()
+    {
+        $data = JWTAuth::user()->products();
+
+        return response()->json(compact('data'));
     }
 }
