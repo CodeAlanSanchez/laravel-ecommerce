3c406d117907c0290c2acf7ddd20890795a31239
Alan Sanchez <realalansan@gmail.com>
2021-07-01 05:53:52 -0700

    Add product fetching route

--- routes/api.php
+++ routes/api.php
@@ -19,5 +19,6 @@ Route::post('login', [App\Http\Controllers\UsersController::class, 'authenticate
 
 Route::group(['middleware' => ['jwt.verify']], function () {
     Route::get('user', [App\Http\Controllers\UsersController::class, 'getAuthenticatedUser']);
-    Route::post('product', [App\Http\Controllers\ProductsController::class, 'store']);
+    Route::get('products', [App\Http\Controllers\ProductsController::class, 'productsByUser']);
+    Route::post('products', [App\Http\Controllers\ProductsController::class, 'store']);
 });
