6858fec2f58f5e432a60e1967db7ba66ec01e452
Alan Sanchez <realalansan@gmail.com>
2021-07-01 05:50:37 -0700

    Add many to one relation for user

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -7,5 +7,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    use HasFactory;
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'image',
+        'price',
+        'category',
+        'discount'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
