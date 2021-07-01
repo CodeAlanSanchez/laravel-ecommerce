a0bdca73e5b05a29425c8963cc197ba8510e3ef9
Alan Sanchez <realalansan@gmail.com>
2021-07-01 05:51:12 -0700

    Add fetching user products when showing Account page

--- resources/js/components/Account/index.jsx
+++ resources/js/components/Account/index.jsx
@@ -1,13 +1,36 @@
+import axios from "axios";
 import React, { useState, useEffect } from "react";
+import { useHistory } from "react-router-dom";
 import Product from "../Store/ProductList/Product";
 import AccountForm from "./AccountForm";
 
 const index = () => {
     const user = JSON.parse(localStorage.getItem("profile"));
+    const [products, setProducts] = useState({});
+    const history = useHistory();
 
     if (!user?.email) {
         return <AccountForm />;
     }
+
+    const fetchProducts = async () => {
+        const { data } = await axios.get("api/products", {
+            headers: {
+                Authentication: "Bearer " + user.token,
+            },
+        });
+        setProducts(data);
+    };
+
+    useEffect(() => {
+        console.log(user);
+        fetchProducts();
+    }, []);
+
+    const handleLogout = () => {
+        localStorage.setItem("profile", JSON.stringify("{}"));
+        history.go(0);
+    };
 
     return (
         <div className="account">
@@ -17,7 +40,9 @@ const index = () => {
                 <h6 className="name">{user?.name}</h6>
                 <h4 className="email subheading">Email</h4>
                 <h6 className="email">{user?.email}</h6>
-                <button className="sm primary">Log Out</button>
+                <button className="sm primary" onClick={() => handleLogout()}>
+                    Log Out
+                </button>
             </div>
             <div className="account-products">
                 <h1 className="products-heading">Your Products</h1>
