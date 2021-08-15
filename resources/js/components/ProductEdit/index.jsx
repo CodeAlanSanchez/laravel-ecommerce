172fa6b311079b35ec29e3648d173381a8f096e5
Alan Sanchez <realalansan@gmail.com>
2021-08-15 02:31:32 -0700

    Fix loading and URI

--- resources/js/components/ProductEdit/index.jsx
+++ resources/js/components/ProductEdit/index.jsx
@@ -38,7 +38,7 @@ const index = ({ match, location }) => {
     const handleSubmit = (e) => {
         e.preventDefault();
 
-        axios.patch("/api/products/{productId}", form, {
+        axios.patch(`/api/products/${product.id}`, form, {
             headers: { Authorization: `Bearer ${user?.token}` },
         });
     };
@@ -49,54 +49,58 @@ const index = ({ match, location }) => {
 
     return (
         <div className="edit">
-            <div className="productForm">
-                <h4 align="center">Edit Product</h4>
-                <form onSubmit={(e) => handleSubmit(e)}>
-                    <label htmlFor="name">Name</label>
-                    <input
-                        type="text"
-                        name="name"
-                        value={form.name}
-                        onChange={(e) =>
-                            setForm({ ...form, name: e.target.value })
-                        }
-                        placeholder="Product Name"
-                    />
-                    <label htmlFor="price">Price</label>
-                    <input
-                        type="text"
-                        value={form.price}
-                        onChange={(e) =>
-                            setForm({ ...form, price: e.target.value })
-                        }
-                        name="price"
-                        placeholder="Price"
-                    />
-                    <label htmlFor="discount">Discount</label>
-                    <input
-                        type="text"
-                        value={form.discount}
-                        onChange={(e) =>
-                            setForm({ ...form, discount: e.target.value })
-                        }
-                        name="discount"
-                        placeholder="Discount"
-                    />
-                    <label htmlFor="category">Category</label>
-                    <input
-                        type="text"
-                        value={form.category}
-                        onChange={(e) =>
-                            setForm({ ...form, category: e.target.value })
-                        }
-                        name="category"
-                        placeholder="Category"
-                    />
-                    <button type="submit" className="sm primary">
-                        Update Product
-                    </button>
-                </form>
-            </div>
+            {loading > 1 ? (
+                <p>Loading...</p>
+            ) : (
+                <div className="productForm">
+                    <h4 align="center">Edit Product</h4>
+                    <form onSubmit={(e) => handleSubmit(e)}>
+                        <label htmlFor="name">Name</label>
+                        <input
+                            type="text"
+                            name="name"
+                            value={form.name}
+                            onChange={(e) =>
+                                setForm({ ...form, name: e.target.value })
+                            }
+                            placeholder="Product Name"
+                        />
+                        <label htmlFor="price">Price</label>
+                        <input
+                            type="text"
+                            value={form.price}
+                            onChange={(e) =>
+                                setForm({ ...form, price: e.target.value })
+                            }
+                            name="price"
+                            placeholder="Price"
+                        />
+                        <label htmlFor="discount">Discount</label>
+                        <input
+                            type="text"
+                            value={form.discount}
+                            onChange={(e) =>
+                                setForm({ ...form, discount: e.target.value })
+                            }
+                            name="discount"
+                            placeholder="Discount"
+                        />
+                        <label htmlFor="category">Category</label>
+                        <input
+                            type="text"
+                            value={form.category}
+                            onChange={(e) =>
+                                setForm({ ...form, category: e.target.value })
+                            }
+                            name="category"
+                            placeholder="Category"
+                        />
+                        <button type="submit" className="sm primary">
+                            Update Product
+                        </button>
+                    </form>
+                </div>
+            )}
         </div>
     );
 };
