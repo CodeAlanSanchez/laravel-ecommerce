399090b1f99130171d7afa1d23fe10affc76758e
Alan Sanchez <realalansan@gmail.com>
2021-10-05 16:46:32 -0700

    Create CartItem component

---
+++ resources/js/components/Cart/CartItem/index.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const index = ({ product }) => {
+    return (
+        <div className="cartItem">
+            {console.log(product)}
+            <img src={product.image} alt={product.name} style={{}} />
+            <h5>{product.name}</h5>
+            {product.discount > 0 ? (
+                <p className="price">
+                    <span className="price old">${product.price}</span>
+                    &nbsp; ${product.discount}
+                </p>
+            ) : (
+                <p className="price">${product.price}</p>
+            )}
+            <p>{product.amount}</p>
+        </div>
+    );
+};
+
+export default index;
