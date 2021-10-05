8399849404eeab9f38da3980c032b3f176bff004
Alan Sanchez <realalansan@gmail.com>
2021-10-05 16:46:39 -0700

    Add cart items to Cart component

--- resources/js/components/Cart/index.jsx
+++ resources/js/components/Cart/index.jsx
@@ -1,11 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import CartItem from "./CartItem";
+import { fetchCart } from "../../actions/cart";
+import { useDispatch, useSelector } from "react-redux";
 
 const index = () => {
+    const cart = useSelector((state) => state.cart);
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(fetchCart);
+    }, []);
+
     return (
         <div className="flex">
             <div className="cart">
                 <h6 className="shopHeader">Cart</h6>
-                <p>Your cart is empty...</p>
+                {cart.length > 0 ? (
+                    <div className="cartItems">
+                        {cart.map((i) => (
+                            <CartItem key={i.id} product={i}></CartItem>
+                        ))}
+                    </div>
+                ) : (
+                    <p>Your cart is empty...</p>
+                )}
             </div>
         </div>
     );
