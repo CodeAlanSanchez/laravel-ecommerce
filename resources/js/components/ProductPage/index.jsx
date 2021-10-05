854a87d41ae137826bba34cf9634744e5ae64f2f
Alan Sanchez <realalansan@gmail.com>
2021-10-05 16:46:18 -0700

    Add product adding functionality

--- resources/js/components/ProductPage/index.jsx
+++ resources/js/components/ProductPage/index.jsx
@@ -1,9 +1,14 @@
 import React, { useState, useEffect } from "react";
+import { addProduct } from "../../actions/cart";
 import axios from "axios";
+import { useHistory } from "react-router-dom";
+import { useDispatch } from "react-redux";
 
 const index = ({ match, location }) => {
     const [product, setProduct] = useState({});
     const [loading, setLoading] = useState(true);
+    const history = useHistory();
+    const dispatch = useDispatch();
     const [user, setUser] = useState(
         JSON.parse(localStorage.getItem("profile"))
     );
@@ -22,18 +27,25 @@ const index = ({ match, location }) => {
         fetchProduct(productId);
     }, []);
 
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        dispatch(addProduct(product));
+        history.push("/cart");
+    };
+
     return (
         <div>
             {loading ? (
-                <>
-                    <p>Loading...</p>
-                </>
+                <div className="flex" style={{ height: "1vh" }}>
+                    <h2>Loading...</h2>
+                </div>
             ) : (
                 <div className="productPage">
                     <div className="image">
                         <img src={product.image} alt="" />
                     </div>
-                    <div className="info">
+                    <form className="info" onSubmit={(e) => handleSubmit(e)}>
                         <h2>
                             {product.name}
                             {user?.id == product.user_id ? (
@@ -60,10 +72,10 @@ const index = ({ match, location }) => {
                         ) : (
                             <p className="price">${product.price}</p>
                         )}
-                        <button className="primary cart disabled" type="button">
+                        <button className="primary cart" type="submit">
                             Add To Cart
                         </button>
-                    </div>
+                    </form>
                 </div>
             )}
         </div>
