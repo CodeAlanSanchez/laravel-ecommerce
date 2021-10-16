9f6b9617a7b30bf6d4b9260fe6fc23b0dc7352a9
Alan Sanchez <realalansan@gmail.com>
2021-10-15 23:12:28 -0700

    Add amount property to items in cart

--- resources/js/reducers/cart.js
+++ resources/js/reducers/cart.js
@@ -3,13 +3,22 @@ export default (cart = [], action) => {
         case "FETCH_CART":
             return action.payload;
         case "ADD_PRODUCT":
-            console.log(cart);
-            localStorage.setItem(
-                "cart",
-                JSON.stringify([...cart, action.payload])
-            );
+            const item = cart.find(({ id }) => id === action.payload.id);
+            let products = [];
 
-            return [...cart, action.payload];
+            if (typeof item !== "undefined") {
+                products = cart.map((i) =>
+                    i.id === item.id
+                        ? { ...action.payload, key: i.id, amount: i.amount + 1 }
+                        : i
+                );
+                localStorage.setItem("cart", JSON.stringify(products));
+            } else {
+                products = [...cart, { ...action.payload, amount: 1 }];
+                localStorage.setItem("cart", JSON.stringify(products));
+            }
+
+            return products;
         case "REMOVE_PRODUCT":
             return cart.filter((item) => item.id !== action.payload.id);
         default:
