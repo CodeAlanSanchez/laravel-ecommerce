a86155fbc74fa874177f8bc3afb8bd23b0fa233e
Alan Sanchez <realalansan@gmail.com>
2021-10-15 22:38:55 -0700

    Store cart items in local storage instead of only in state

--- resources/js/reducers/cart.js
+++ resources/js/reducers/cart.js
@@ -1,22 +1,15 @@
 export default (cart = [], action) => {
     switch (action.type) {
         case "FETCH_CART":
-            return cart;
+            return action.payload;
         case "ADD_PRODUCT":
-            let inArray = cart.find((i) => i.id === action.payload.id);
-            let amount = 1;
+            console.log(cart);
+            localStorage.setItem(
+                "cart",
+                JSON.stringify([...cart, action.payload])
+            );
 
-            if (inArray > 0) {
-                cart.map((i) => {
-                    if (i.id === action.payload.id) {
-                        console.log("ITEM FOUND!!!");
-                        amount = i.amount += 1;
-                    }
-                });
-            }
-            console.log("ITEM NOT FOUND!!!");
-
-            return [...cart, { ...action.payload, amount }];
+            return [...cart, action.payload];
         case "REMOVE_PRODUCT":
             return cart.filter((item) => item.id !== action.payload.id);
         default:
